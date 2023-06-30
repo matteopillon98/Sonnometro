1d04aa94512a796ef45b5bcd9fc6b4277b92db48
Matteo Pillon <matteopillon98@gmail.com>
2023-06-30 23:58:50 +0200

    fix cors 2

--- api/index.js
+++ api/index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
 const port = 3001;
 app.use(cors()); // Abilita le richieste CORS per tutte le rotte
 
-const TelegramBot = require("node-telegram-bot-api");
-
-const botToken = "6237272691:AAG8QimxSfpDpNpYR8mDX1MLNDAF-nTirOc";
-const bot = new TelegramBot(botToken, { polling: false });
-
 // Middleware per il parsing del corpo della richiesta
 app.use(bodyParser.json());
 
-const chatId = "286242051"; // L'ID della chat a cui desideri inviare il messaggio
-
 // Rotta per la ricezione dei dati del sonno
 app.post("/api/sonno", (req, res) => {
+  const chatId = "286242051"; // L'ID della chat a cui desideri inviare il messaggio
+  const botToken = "6237272691:AAG8QimxSfpDpNpYR8mDX1MLNDAF-nTirOc";
+  const bot = new TelegramBot(botToken, { polling: false });
   const sonno = req.body.sonno;
   const message = "Livello di sonno impostato: " + sonno;
   console.log("Dati del sonno ricevuti:", sonno);
