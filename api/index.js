0a2a4b0929f6fbb6c57998de6e463bcc95ce447b
Matteo Pillon <matteopillon98@gmail.com>
2023-07-10 14:06:24 +0200

    fix server telegram app

--- api/index.js
+++ api/index.js
@@ -10,11 +10,12 @@ app.use(cors()); // Abilita le richieste CORS per tutte le rotte
 // Middleware per il parsing del corpo della richiesta
 app.use(bodyParser.json());
 
+const chatId = "286242051"; // L'ID della chat a cui desideri inviare il messaggio
+const botToken = "6237272691:AAG8QimxSfpDpNpYR8mDX1MLNDAF-nTirOc";
+const bot = new TelegramBot(botToken, { polling: false });
+
 // Rotta per la ricezione dei dati del sonno
 app.post("/api/sonno", (req, res) => {
-  const chatId = "286242051"; // L'ID della chat a cui desideri inviare il messaggio
-  const botToken = "6237272691:AAG8QimxSfpDpNpYR8mDX1MLNDAF-nTirOc";
-  const bot = new TelegramBot(botToken, { polling: false });
   const sonno = req.body.sonno;
   const message = "Livello di sonno impostato: " + sonno;
   console.log("Dati del sonno ricevuti:", sonno);
